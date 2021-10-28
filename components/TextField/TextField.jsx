91e3bc18af8a879207a10f180f0cf7fe61f703d1
Talles Corradini <tallescorradini@gmail.com>
2021-10-28 13:05:57 -0300

    feat: display error message

--- components/TextField/TextField.jsx
+++ components/TextField/TextField.jsx
@@ -1,10 +1,26 @@
 import styles from "./TextField.module.css";
 
-export function TextField({ label, name, children, ...rest }) {
+export function TextField({
+  label = "",
+  name,
+  errors = [],
+  children,
+  ...rest
+}) {
+  const hasError = errors.length > 0;
+
   return (
     <div className={styles.field}>
       <label htmlFor={name}>{label}</label>
       <input name={name} id={name} type="text" {...rest} />
+
+      {hasError ? (
+        <ul className={styles.errorMessage}>
+          {errors.map((e) => (
+            <li key={e}>{`${e}. `}</li>
+          ))}
+        </ul>
+      ) : null}
     </div>
   );
 }
