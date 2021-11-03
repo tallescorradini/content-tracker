e3fb2c558e8ad7e9458a3db3254f736d95feed03
Talles Corradini <tallescorradini@gmail.com>
2021-11-03 09:44:06 -0300

    feat(Button): add fullwidth property

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,10 +1,17 @@
 import styles from "./Button.module.css";
 
-export function Button({ variant, children, type = "button", ...rest }) {
+export function Button({
+  variant,
+  children,
+  type = "button",
+  fullWidth = false,
+  ...rest
+}) {
   return (
     <button
       className={`${styles.button} ${styles[variant]}`}
       type={type}
+      style={{ width: fullWidth ? "100%" : "auto" }}
       {...rest}
     >
       {children}
