6b26a96c9970893727c5cc832db4ed742127f170
Talles Corradini <tallescorradini@gmail.com>
2021-11-03 11:27:04 -0300

    feat(api/favorites): add video count for each channel

--- pages/api/favorites.js
+++ pages/api/favorites.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   if (req.method === "GET") {
     const { channelId } = req.query;
 
-    const URL = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=${process.env.YOUTUBE_API_KEY}`;
+    const URL = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&id=${channelId}&key=${process.env.YOUTUBE_API_KEY}`;
 
     try {
       const { data } = await axios.get(URL);
@@ -17,6 +17,7 @@ export default async function handler(req, res) {
         title: list[0].snippet.title,
         thumbnail: list[0].snippet.thumbnails.default,
         url: `https://www.youtube.com/channel/${list[0].id}`,
+        videoCount: list[0].statistics.videoCount,
       };
 
       res.status(200).json(channelData);
